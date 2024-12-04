935b283db13b17e2b418d12d4169ebba2c79a416
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-12-04 18:09:55 -0300

    feat(backend/frontend): Save

--- server/src/app.js
+++ server/src/app.js
@@ -1,40 +1,40 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import routes from './routes/index.js'
+// import express from 'express'
+// import bodyParser from 'body-parser'
+// import cors from 'cors'
+// import routes from './routes/index.js'
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  process.env.CLIENT_CORS_ALLOW_URL
-]
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   process.env.CLIENT_CORS_ALLOW_URL
+// ]
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Bloqueado pelo CORS'))
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-  optionsSuccessStatus: 200
-}
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Bloqueado pelo CORS'))
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// }
 
-const app = express()
+// const app = express()
 
-// CORS
-app.use(cors(corsOptions))
+// // CORS
+// app.use(cors(corsOptions))
 
-// Middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// // Middleware
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 
-// Routes
-app.use('/api', routes)
+// // Routes
+// app.use('/api', routes)
 
-app.get('/test', (req, res) => {
-  res.json({ message: 'Teste bem-sucedido! O servidor está funcionando.' })
-})
+// app.get('/test', (req, res) => {
+//   res.json({ message: 'Teste bem-sucedido! O servidor está funcionando.' })
+// })
 
-export default app
+// export default app
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,37 @@
-import app from './app.js'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import { connectDatabase } from './config/database.js'
+import routes from './routes/index.js'
 
 const PORT = process.env.PORT || 5000
+
+const allowedOrigins = ['http://localhost:5173', 'https://dominio-prod.com']
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Bloqueado pelo CORS'))
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  optionsSuccessStatus: 200
+}
+
+const app = express()
+
+// CORS
+app.use(cors(corsOptions))
+
+// Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Routes
+app.use('/api', routes)
 
 connectDatabase()
   .then(() => {
